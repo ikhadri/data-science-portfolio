57e4e7cfde2c1712a2cc602352cbc671e69e17d1
ikhadri <ikhadri05@gmail.com>
2026-09-13 14:24:53 -0700

    added ask_age to calculate birth year of user

--- projects/greeting-project/hello.py
+++ projects/greeting-project/hello.py
@@ -1,6 +1,11 @@
 # My first Python project in GitHub
 def greet_user():
-name = input("Enter your name: ")
-print(f"Hello, {name}! Welcome to Data Science.")
+    name = input("Enter your name: ")
+    print(f"Hello, {name}! Welcome to Data Science.")
+def ask_age():
+    age = int(input("Enter your age: "))
+    date_of_birth_year = 2026 - age
+    print (f"You were born in {date_of_birth_year}. ")
 if __name__ == "__main__":
-greet_user()
+    greet_user()
+    ask_age()

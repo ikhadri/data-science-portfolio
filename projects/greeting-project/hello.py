6e6d14e226e2756d0e75f0ac730352c2d3d457a9
ikhadri <ikhadri05@gmail.com>
2026-09-13 14:28:39 -0700

    Add name_a_fruit function that asks the user to enter a fruit name

--- projects/greeting-project/hello.py
+++ projects/greeting-project/hello.py
@@ -6,6 +6,10 @@ def ask_age():
     age = int(input("Enter your age: "))
     date_of_birth_year = 2026 - age
     print (f"You were born in {date_of_birth_year}. ")
+def name_a_fruit():
+    fruit = input("Name a fruit for fun: ")
+    print(f"You named this {fruit}.")
 if __name__ == "__main__":
     greet_user()
     ask_age()
+    name_a_fruit()
